a7f762def8f3f37351df6346c7d2ac88bfc177f7
Vincent Yang <missuo@pm.me>
2025-12-13 21:39:36 +0800

    Initial Commit

---
+++ Mist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mist
+//
+//  Created by Vincent Yang on 12/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mist/MistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MistApp.swift
+//  Mist
+//
+//  Created by Vincent Yang on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
